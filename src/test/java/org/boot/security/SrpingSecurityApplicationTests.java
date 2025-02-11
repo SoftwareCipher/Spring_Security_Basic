636aff35fc251ad59120a96c124a799a1fbb7cdf
observerUA <kirillkuznetsov181@gmail.com>
2025-02-11 16:18:40 +0200

    start study spring security

---
+++ src/test/java/org/boot/security/SrpingSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boot.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
